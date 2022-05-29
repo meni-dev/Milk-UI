15fe02f8d9a4ecb84271eb50f347a1a66cb6cae4
menisaran15@gmail.com <menisaran15@gmail.com>
2022-05-29 17:26:21 +0530

    add customer form design completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddCustomerComponent } from './customer/add-customer/add-customer.component';
+import { DeleteCustomerComponent } from './customer/delete-customer/delete-customer.component';
+import { EditCustomerComponent } from './customer/edit-customer/edit-customer.component';
+import { ListCustomerComponent } from './customer/list-customer/list-customer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'add-customer', component: AddCustomerComponent },
+  { path: 'edit-customer', component: EditCustomerComponent },
+  { path: 'list-customer', component: ListCustomerComponent },
+  { path: 'delete-customer', component: DeleteCustomerComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddCustomerComponent } from './customer/add-customer/add-customer.component';
+import { EditCustomerComponent } from './customer/edit-customer/edit-customer.component';
+import { ListCustomerComponent } from './customer/list-customer/list-customer.component';
+import { DeleteCustomerComponent } from './customer/delete-customer/delete-customer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddCustomerComponent,
+    EditCustomerComponent,
+    ListCustomerComponent,
+    DeleteCustomerComponent
   ],
   imports: [
     BrowserModule,
